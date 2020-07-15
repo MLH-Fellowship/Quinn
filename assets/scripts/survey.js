66c9654811e21a15437e5eda6bafa9f0ba41ce75
Elizabeth Tackett <emctackett@gmail.com>
2020-07-15 14:05:43 -0500

    refactor event handler script

--- assets/scripts/survey.js
+++ assets/scripts/survey.js
@@ -1,40 +1,50 @@
-$(function() {
-  $('.radio-img').click(function(e) {
-    $(e.target).closest('formset').find('.radio-img').each(function(img) {
-      $(this).removeClass('selected');
-    });
-
-    $(e.target).prev().prop('checked', true).trigger('click');
-    $(e.target).addClass('selected');
+function radioSelection(e) {
+  $(e.target).closest('formset').find('.radio-img').each(function(img) {
+    $(this).removeClass('selected');
   });
 
-  $('.checkbox-img').click(function(e) {
-    let $clickedImg = $(e.target);
-    let sisterImages = $clickedImg.closest('formset').find('.checkbox-img');
-    let selectedCount = 0;
+  $(e.target).prev().prop('checked', true).trigger('click');
+  $(e.target).addClass('selected');
+}
 
-    if ($clickedImg[0].classList.contains('selected')) {
-      $clickedImg.removeClass('selected');
-      return;
+function checkboxSelection(e) {
+  let $clickedImg = $(e.target);
+  let sisterImages = $clickedImg.closest('formset').find('.checkbox-img');
+  let selectedCount = 0;
+
+  if ($clickedImg[0].classList.contains('selected')) {
+    $clickedImg.removeClass('selected');
+    return;
+  }
+
+  sisterImages.each(function(img) {
+    if (this.classList.contains('selected')) {
+      selectedCount += 1;
     }
+  });
 
+  if (selectedCount >= 3) {
     sisterImages.each(function(img) {
-      if (this.classList.contains('selected')) {
-        selectedCount += 1;
-      }
+      $(this).removeClass('selected');
+      $(this).prev().prop('checked', false);
     });
+  }
 
-    if (selectedCount >= 3) {
-      sisterImages.each(function(img) {
-        $(this).removeClass('selected');
-        $(this).prev().prop('checked', false);
-      });
-    }
+  $clickedImg.addClass('selected');
+}
 
-    $clickedImg.addClass('selected');
-  });
+function submitSurvey(e) {
+  e.preventDefault();
+  $('form').submit();
+}
 
-  $('.submit-survey').click(function(e) {
-    $('form').submit();
-  });
+function bindEvents() {
+  $('.radio-img').click(radioSelection);
+  $('.checkbox-img').click(checkboxSelection);
+  $('.submit-survey').click(submitSurvey);
+}
+
+// on DOM ready event, bind event handlers
+$(function() {
+  bindEvents();
 });
