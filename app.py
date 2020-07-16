debe86f2e797cc5eb503d04e37ce94c14fa990aa
Elizabeth Tackett <emctackett@gmail.com>
2020-07-16 15:20:36 -0500

    add error handling routes

--- app.py
+++ app.py
@@ -7,13 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # Landing page for Quinn :)
-    return render_template('index.html', title='Home')
+    return render_template('index.html')
 
 
-@app.route('/survey/')
+@app.route('/survey', methods=['GET', 'POST'])
 def survey():
-    return render_template('survey.html', title='Home')
+    if request.method == 'GET':
+        return render_template('survey.html')
 
+@app.route('/recommend')
+def recommend():
+    return render_template('recommend.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('500.html'), 404
 
 if __name__ == '__main__':
     app.run()
