5b41f36a7199efd929b5d1acec1db4d1f80f1417
Ebonne Cabarrus <42730922+ebonnecab@users.noreply.github.com>
2020-07-17 06:56:32 -0700

    altered model and added backend is mostly done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, url_forß
 from flask_wtf import Form
 from wtforms import IntegerField, StringField, SubmitField, SelectField, DecimalField
 from wtforms.validators import Required
@@ -18,20 +18,30 @@ example = np.array([40, 1, 0, 1, 0, 1, 4]).reshape(1, 7)
 print(model.predict(example))
 
 @app.route('/')
-def hello_world():
+def home():
     # Landing page for Quinn :)
     return render_template('index.html')
 
 
 @app.route('/survey', methods=['GET', 'POST'])
 def survey():
+    print(session)
     if request.method == 'GET':
         return render_template('survey.html')
-    form = models.SurveyForm()
+        
+    form = models.SurveyForm(csrf_enabled=False)
     if request.method == 'POST':
         if form.validate():
-            print('Done!')
-        return render_template('recommend.html')
+            session['price'] = form.q3.data
+            session['skin_type'] = form.q1.data
+            session['product_type'] = form.q_2.data
+
+            survey_instance= [(session['price']), (session['skin_type']), (session['product_type'])]
+
+        #TODO: add lines to return model recommendations
+
+            return redirect(url_for('home')
+        return render_template('recommend.html', form=form, **session)
 
 @app.route('/recommend')
 def recommend():
--- database/models.py
+++ database/models.py
@@ -2,16 +2,15 @@ from .db import db
 from wtforms import Form, fields, validators
 
 class SurveyForm(Form):
-    skin_types = ('Acne Prone', 'Dry', 'Oily', 'Sensitive', 'Combo', 'Normal')
-    skin_concerns = ('Dryness', 'Acne', 'Large Pores', 'Oiliness', 'Aging', 
-    'Razor Bumps', 'Redness', 'Sun damage')
-    product_types=('Cleanser', 'Moisturizer', 'SPF', 'Shaving Cream', 
-    'Deoderant', 'Facial Hair Care', 'Face Masks')
+    skin_types = ('Combination', 'Dry', 'Oily','Combo', 'Normal', 'Sensitive')
+    product_types=('Cleanser', 'Moisturizer', 'SPF', 
+    'Face Masks')
     budget = ('$25 or Less', '$25-50', 'Any Amount')
-    scent_type = ('Citrus', 'Fresh', 'Woodsy', 'None')
 
-    q_1 = fields.SelectField(max_length=3, choices=skin_types)
-    q_3 = fields.SelectField(max_length=3, choices=skin_concerns)
-    q_4 = fields.SelectField(max_length=3, choices=product_types)
-    q_5 = fields.SelectField(max_length=1, choices=budget)
-    q_6 = fields.SelectField(max_length=3, choices=scent_type)
+
+    q_1 = fields.SelectField(max_length=3, choices=skin_types, validators=[Required()])
+    q_2 = fields.SelectField(max_length=3, choices=product_types, validators=[Required()])
+    q_3 = fields.SelectField(max_length=1, choices=budget, validators=[Required()])
+ 
+
+    submit = SubmitField('Submit')
